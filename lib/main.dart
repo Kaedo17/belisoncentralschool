635df671a40be2542249afa632f293783185d4c8
Kaedo17 <92620393+Kaedo17@users.noreply.github.com>
2024-07-21 18:42:34 +0800

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 
 import 'package:belisoncentralschool/auth/login_or_register.dart';
+import 'package:belisoncentralschool/firebase_options.dart';
 import 'package:belisoncentralschool/themes/dark_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
