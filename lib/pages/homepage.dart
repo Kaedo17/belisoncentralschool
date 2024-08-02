ddabed77bef02bf4df89b385089e95e5947ea51e
Kaedo17 <92620393+Kaedo17@users.noreply.github.com>
2024-08-02 23:22:36 +0800

    yesss

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +10,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // logout
+  void logout() async {
+      await FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('mepelove'),
+      bottomNavigationBar: GNav(tabs: [
+        GButton(icon: Icons.home,
+        text: 'Home',
+        ),
+        GButton(icon: Icons.chat,
+        text: "Message",
+        ),
+        GButton(icon: Icons.person_rounded,
+        text: "Profile",
+        ),
+        GButton(icon: Icons.logout, 
+        onPressed: logout,
+        text: "Logout",
+        ),
+      ]),
+
+      body: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
       ),
     );
   }
